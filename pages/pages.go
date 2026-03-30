651114c33d5b9a52e8e7ae3aa1e4ac96ec09b517
Shakil60 <khaldishakil@gmail.com>
2026-03-30 10:32:59 +0200

    morse/html

--- pages/pages.go
+++ pages/pages.go
@@ -13,3 +13,11 @@ func AccueilHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Erreur serveur", http.StatusInternalServerError)
 	}
 }
+
+func MorseHandler(w http.ResponseWriter, r *http.Request) {
+	err := templates.Tpl.ExecuteTemplate(w, "morse", nil)
+	if err != nil {
+		log.Println("Erreur template accueil:", err)
+		http.Error(w, "Erreur serveur", http.StatusInternalServerError)
+	}
+}
