46f7378508518873ec3e69c9d4200978b62c9f00
jfrbast <bastien.jouffre@ynov.com>
2026-03-30 13:54:28 +0200

    page

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 func main() {
 	templates.InitTemplates()
 	http.HandleFunc("/", pages.AccueilHandler)
+	http.HandleFunc("/porte", pages.PorteHandler)
+
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	fmt.Println("Le serveur est lancé http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
--- pages/pages.go
+++ pages/pages.go
@@ -13,6 +13,13 @@ func AccueilHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Erreur serveur", http.StatusInternalServerError)
 	}
 }
+func PorteHandler(w http.ResponseWriter, r *http.Request) {
+	err := templates.Tpl.ExecuteTemplate(w, "porte", nil)
+	if err != nil {
+		log.Println("Erreur template porte:", err)
+		http.Error(w, "Erreur serveur", http.StatusInternalServerError)
+	}
+}
 
 func MorseHandler(w http.ResponseWriter, r *http.Request) {
 	err := templates.Tpl.ExecuteTemplate(w, "morse", nil)
