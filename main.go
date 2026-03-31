a56d86af34e472c5922c046f19c1028a3125049e
adib-houssine <houssinadib39@gmail.com>
2026-03-31 09:13:02 +0200

    test

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 func main() {
 	templates.InitTemplates()
 	http.HandleFunc("/", pages.AccueilHandler)
-<<<<<<< HEAD
+// 0199764e8ea55f7e62eb0917bc40e7fa79edf1dd
 	http.HandleFunc("/enigme4", pages.Enigme4Handler)
-=======
-	http.HandleFunc("/morse", pages.MorseHandler)
-	http.HandleFunc("/porte", pages.PorteHandler)
 
->>>>>>> 0199764e8ea55f7e62eb0917bc40e7fa79edf1dd
+	http.HandleFunc("/morse", pages.MorseHandler) // TODO: Implement MorseHandler in pages package
+	http.HandleFunc("/porte", pages.PorteHandler) // TODO: Implement PorteHandler in pages package
+
+// 0199764e8ea55f7e62eb0917bc40e7fa79edf1dd
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	fmt.Println("Le serveur est lancé http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
