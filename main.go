790a1dc5a8d53a886916d3385cb3f7048035afea
jfrbast <bastien.jouffre@ynov.com>
2026-03-31 13:25:29 +0200

    merge

--- main.go
+++ main.go
@@ -13,12 +13,9 @@ func main() {
 	http.HandleFunc("/challenge48h/bureau", pages.Enigme4Handler)
 	http.HandleFunc("/challenge48h/morse", pages.MorseHandler)
 	http.HandleFunc("/challenge48h/porte", pages.PorteHandler)
-<<<<<<< HEAD
 	http.HandleFunc("/challenge48h/suspect", pages.SuspectHandler)
-=======
 	http.HandleFunc("/challenge48h/bombe", pages.BombeHandler)
 	http.HandleFunc("/challenge48h/appartement", pages.AppartHandler)
->>>>>>> d9f3f20b89456883136db47f0c35015172e76c61
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	fmt.Println("Le serveur est lancé http://localhost:8080")
